e7e362c7d4b6091aa26a47a422b8d95be74465dd
santhitak <santhita.sk@gmail.com>
2022-09-12 01:16:10 +0700

    separated

--- src/main/java/com/example/lab04service/views/MathAPI/MathAPI.java
+++ src/main/java/com/example/lab04service/views/MathAPI/MathAPI.java
@@ -3,8 +3,9 @@ package com.example.lab04service.views.MathAPI;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.router.Route;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MathAPI extends VerticalLayout {
 
     public MathAPI(){
+        Div container = new Div();
+        container.setWidth("40vw");
+        container.setHeight("100%");
+
         FormLayout calculator = new FormLayout();
-        TextField n1 = new TextField("Number 1");
-        TextField n2 = new TextField("Number 2");
-        TextField n3 = new TextField("Answer");
+        NumberField n1 = new NumberField("Number 1");
+        NumberField n2 = new NumberField("Number 2");
+        NumberField n3 = new NumberField("Answer");
 
         FormLayout btnForm = new FormLayout();
         Button btnPlus = new Button("+");
@@ -25,6 +30,13 @@ public class MathAPI extends VerticalLayout {
         Button btnDivide = new Button("/");
         Button btnMod = new Button("Mod");
         Button btnMax = new Button("Max");
+
+        btnPlus.addClickListener(e -> {this.myPlus(n1.getValue(), n2.getValue());});
+        btnMinus.addClickListener(e -> {this.myMinus(n1.getValue(), n2.getValue());});
+        btnMulti.addClickListener(e -> {this.myMulti(n1.getValue(), n2.getValue());});
+        btnDivide.addClickListener(e -> {this.myDivide(n1.getValue(), n2.getValue());});
+        btnMod.addClickListener(e -> {this.myMod(n1.getValue(), n2.getValue());});
+        btnMax.addClickListener(e -> {this.myMax(n1.getValue(), n2.getValue());});
 
         btnForm.add(btnPlus, btnMinus, btnMinus, btnMulti, btnDivide, btnMod, btnMax);
 
@@ -41,7 +53,8 @@ public class MathAPI extends VerticalLayout {
 
         calculator.setColspan(btnForm, 2);
 
-        add(calculator);
+        container.add(calculator);
+        add(container);
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.START);
@@ -50,6 +63,7 @@ public class MathAPI extends VerticalLayout {
 
     @RequestMapping("/plus/{n1}/{n2}")
     public double myPlus(@PathVariable Double n1, @PathVariable Double n2){
+
         return n1+n2;
     }
 
@@ -71,5 +85,10 @@ public class MathAPI extends VerticalLayout {
     @RequestMapping("/mod/{n1}/{n2}")
     public double myMod(@PathVariable Double n1, @PathVariable Double n2){
         return n1%n2;
+    }
+
+    @RequestMapping("/max/{n1}/{n2}")
+    public double myMax(@PathVariable Double n1, @PathVariable Double n2){
+        return Math.max(n1, n2);
     }
 }
